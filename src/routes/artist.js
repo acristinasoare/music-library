fa9a2c4c2f2be336b082817786a2c07a3043b121
Cristina Soare <kiti.soare@ymail.com>
2023-08-04 18:46:34 +0100

    minimal code update based on code review

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -12,15 +12,10 @@ const { createAlbum } = require("../controllers/album");
 const artistRouter = express.Router();
 
 artistRouter.post("/", createArtist);
-
 artistRouter.get("/", readArtists);
-
 artistRouter.get("/:id", readArtistById);
-
 artistRouter.patch("/:id", updateArtist);
-
 artistRouter.delete("/:id", deleteArtistById);
-
 artistRouter.post("/:artistId/albums", createAlbum);
 
 module.exports = { artistRouter };
